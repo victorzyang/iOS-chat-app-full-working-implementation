7c7cd4f7a2340e9d37c0739e304421312379b4ba
Yue <yuezhang5@cmail.carleton.ca>
2018-04-07 21:45:30 -0400

    Initial Commit

---
+++ Assignment4B-iOSWebsockets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment4B-iOSWebsockets
+//
+//  Created by Yue Zhang on 2018-04-07.
+//  Copyright © 2018 COMP2601. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
